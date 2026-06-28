e21345002b0fa2511089b2bde358e6a1db335db9
ksupriya98 <supriyak98.sk@gmail.com>
2026-06-28 20:07:02 +0530

    Initial commit

---
+++ WebApiAssign1/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Employee
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string City { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string State { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Country { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string EmailId { get; set; }
+
+    [Required]
+    [Phone]
+    public string PhoneNo { get; set; }
+}
